ea60a2ab129efc21f5d0257306b7fd4244f936c5
Your Name <your_email@example.com>
2026-05-04 18:16:59 -0400

    first python file

--- Pre-Week-3/Week-05/Ex1A_hello.py
+++ Pre-Week-3/Week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
-print('Hello world!')
+print("Hello world!")
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world" prints twice because we called print() two times
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# The result is 3.5 because Python adds them as numbers (3 + 0.50)
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
